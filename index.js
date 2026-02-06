b3138c501e7d493166e40534a3c139ed86c549fb
Fagner Silva <familiafeynoordy@gmail.com>
2026-02-06 01:57:02 -0300

    ci: add backend source and dockerfile for ECR build

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ status: "Backend Igreja OK 🙌" });
+});
+
+app.listen(3000, () => {
+  console.log("Backend rodando na porta 3000");
+});
